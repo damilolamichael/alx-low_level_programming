bdf79aa907df57ca23a261939cb5be6fed6018a4
damilolamichael <damilolamichaels@gmail.com>
2023-09-07 20:26:18 -0700

    100-intel and 101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - This c program prints a line to the standard error
+ * Return 1
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
