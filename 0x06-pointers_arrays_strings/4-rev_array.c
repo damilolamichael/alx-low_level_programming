ec45750fdeac6d719226a366f8d5c8c6acca1d77
damilolamichael <damilolamichaels@gmail.com>
2023-10-17 03:26:07 -0700

    Changes all lower letter of a string to upper case

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * reverse_array - The main faunction
+  *
+  * @a: Function parameter
+  *
+  * @n: Function parameter
+  *
+  * Return: Always 0
+  */
+void reverse_array(int *a, int n)
+{
+	int k;
+	int r;
+	
+	for (k = 0; k < n / 2 ; k++)
+	{
+	r = a[k];
+	a[k] = a[n - 1 - k];
+	a[n - 1 - k ] = r;
+	}
+}
